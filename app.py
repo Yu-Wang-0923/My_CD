5833f7ca7db755dbdb6247cd0ef3ad92ffcc8db9
Yuwang923 <yuwang2024@ruc.edu.cn>
2025-11-17 17:23:17 +0800

    homepage design

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="EA",
+    page_icon="☺️",
+    layout="wide",
+)
+
+st.title("encyclopedic app")
+st.sidebar.success("Select a page above.")
+
 st.write("hello world!")
